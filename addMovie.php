cd4c6c019c34d3a792a2020aa50faa0e4c000471
Ong Yi <oyyi.23@ichat.sp.edu.sg>
2025-01-26 16:15:26 +0800

    Created Add movie page

---
+++ addMovie.php
@@ -0,0 +1,39 @@
+<h1 class="headers">Add Movie:</h1>
+<form class="form" method="post" >
+    <div>
+        <label for="banner" class="banner-style" id="banner-style">
+            <img src="css/cloud-upload.png" width="50" id="preview-banner" alt="Upload Preview">
+            <p>Upload Movie Banner</p>
+        </label>
+        <input type="file" id="banner" accept="image/png, image/jpg">
+    </div>
+    <div style="margin-top:20px">
+        <label for="poster" class="poster-style" id="poster-style">
+            <img src="css/cloud-upload.png" width="50" id="preview-poster" alt="Upload Preview">
+            <p>Upload Movie Poster</p>
+        </label>
+        <input type="file" id="poster" accept="image/png, image/jpg">
+    </div>
+    <div style="margin-top:20px; display: flex; align-items: center;">
+    <label style="width: 150px;">Movie Name:</label>
+    <input type="text" class="name" >
+</div>
+<div style="margin-top:20px; display: flex; align-items: center;">
+    <label style="width: 150px;">Movie Description:</label>
+    <textarea class="desc"></textarea>
+</div>
+    <div style="margin-top:20px">Movie Rating: 
+    <input type="radio" name="movieRating" id="PG" value="PG" checked>
+    <label for="PG" class="rating">PG</label>
+    <input type="radio" name="movieRating" id="PG13" value="PG13">
+    <label for="PG13" class="rating">PG13</label>
+    <input type="radio" name="movieRating" id="NC16" value="NC16">
+    <label for="NC16" class="rating">NC16</label>
+    <input type="radio" name="movieRating" id="M18" value="M18">
+    <label for="M18" class="rating">M18</label>
+    <input type="radio" name="movieRating" id="R21" value="R21">
+    <label for="R21" class="rating">R21</label>
+    </div>
+    <div><button class="button" name="add" value="add_movie">Add Movie</button></div>
+    
+</form>
--- admin.php
+++ admin.php
@@ -1,11 +1,10 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Absolute Cinema</title>
-    <link rel="stylesheet" href="css/adminstyle.css">
+    <link rel="stylesheet" href="css/styles.css">
     
     
 </head>
@@ -26,5 +25,6 @@
             include 'homepage.php';
         }
         ?>
+        <script src="javascript/script.js"></script>
 </body>
 </html>
